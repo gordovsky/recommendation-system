05c011aa43403c369d9de94d338525e82c1f64e9
Denis Gordovsky <gordovsky.d@gmail.com>
2017-03-14 23:48:14 +0200

    nbc

--- recommendation system/Program.cs
+++ recommendation system/Program.cs
@@ -7,7 +7,6 @@ using System.Text.RegularExpressions;
 using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.FileIO;
 using System.Data;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace recommendation_system
 {
@@ -16,44 +15,34 @@ namespace recommendation_system
         static void Main(string[] args)
         {
 
+            NBC nbc = new NBC();
+            nbc.Verdict();
+
+            //var x = nbc.Probability(true);
+            //var y = nbc.Probability(false);
+
+            Console.ReadKey();
 
             //var reddit = new Reddit(@"C:\Users\col403\Desktop\reddit.csv");
             //reddit.ReadData();
             //reddit.RankNews();
             //reddit.Print();
 
-            Task3 task = new Task3(@"C:\Users\Admin\Desktop\ml-latest-small\movies.csv",
-                                   @"C:\Users\Admin\Desktop\ml-latest-small\ratings.csv");
+            //Task3 task = new Task3(@"C:\Users\col403\Desktop\ml-latest-small\movies.csv",
+            //                       @"C:\Users\col403\Desktop\ml-latest-small\ratings.csv");
 
             //var dm = task.DampedMeanCollection();
             //var dampedTop10 = dm.OrderByDescending(x => x.Value).Take(10);
-            //foreach (var d in dampedTop10)
+            //foreach(var d in dampedTop10)
             //{
             //    string similar = task.GetMovieName(task.GetMostSimilar(d.Key));
-            //    Console.WriteLine("{0} damped mean: {1}. Similar to {2}", task.GetMovieName(d.Key), d.Value, similar);
+            //    Console.WriteLine("{0} damped mean: {1}. Similar to {2}",task.GetMovieName(d.Key), d.Value, similar);
             //}
 
             //var fdsx = task.SimilarityValue(4324, 4234);
 
-            //string similarM = task.GetMovieName(task.GetMostSimilar(1732));
-            ConsoleKeyInfo input;
-            do
-            {
-                Console.WriteLine("For what movie do you need recommendation, my master?");
-                Int64 id = Int64.Parse(Console.ReadLine());
-                try
-                {
-                    string similarM = task.GetMovieName(task.GetMostSimilar(id));
-                    Console.WriteLine("Try \"{0}\"", similarM);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("error...");
-                }
-                input = Console.ReadKey();
-            } while (input.Key != ConsoleKey.Escape);
-            
-            Console.ReadKey();
+            //string similar = task.GetMovieName(task.GetMostSimilar(8));
+            //Console.ReadKey();
         }
     }
 }
